82c0afea4dfed0aa19449c741c273528ce7ff6f7
Caio Tadashi <ctdsh@icloud.com>
2021-05-16 18:06:20 -0300

    added log

--- js/app.js
+++ js/app.js
@@ -5,9 +5,11 @@ const booFace = document.getElementById('boo')
 hiddenFace.addEventListener('mousedown',() => {
     hiddenFace.classList.remove('active');
     booFace.classList.add('active');
+    console.log('click');
 });
 
 booFace.addEventListener('mouseup',() => {
     booFace.classList.remove('active');
     hiddenFace.classList.add('active');
+    console.log('unclick');
 });
